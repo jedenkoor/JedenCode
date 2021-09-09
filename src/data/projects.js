20bba3b0a8fd995b3158d1d904307cc7515afe4c
Daniil Kuzmin <dazekamikadze19962002@gmail.com>
2021-09-09 17:56:20 +0300

    remove RA projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,124 +1,124 @@
 export default [
-  {
-    title: 'Internation',
-    description: 'Спортивный бренд одежды',
-    link: 'https://internation.jedenkoor.ru/',
-    image: '/images/projects/internation.svg',
-    tags: ['Gulp', 'jQuery', 'Интернет-магазин'],
-    date: '2019.03.25'
-  },
-  {
-    title: 'Малина',
-    description: 'Художественная мастерская по созданию и росписи матрешёк',
-    link: 'https://matreshki.jedenkoor.ru/',
-    image: '/images/projects/matreshki.png',
-    tags: ['Gulp', 'jQuery', 'Сайт-визитка'],
-    date: '2019.02.18'
-  },
-  {
-    title: 'Clevergrad Premium',
-    description: 'Элитная загородная недвижимость',
-    link: 'https://clever-elite.jedenkoor.ru/',
-    image: '/images/projects/clever-elite.svg',
-    tags: ['Gulp', 'jQuery', 'Интернет-витрина', 'Анимации'],
-    date: '2019.05.30'
-  },
-  {
-    title: 'Instafilter',
-    description: 'Студия разработки AR игр и масок для Instagram, TikTok, Snapchat и VK',
-    link: 'https://instafilter.jedenkoor.ru/',
-    image: '/images/projects/instafilter.svg',
-    tags: ['Gulp', 'jQuery', 'Сайт-визитка', 'Анимации'],
-    date: '2019.08.13'
-  },
-  {
-    title: 'Levitacia',
-    description: 'Премиальная одежда',
-    link: 'https://levitation.jedenkoor.ru/',
-    image: '/images/projects/levitacia.png',
-    tags: ['Gulp', 'jQuery', 'Интернет-магазин'],
-    date: '2019.10.18'
-  },
-  {
-    title: 'RA-Studio',
-    description: 'Продажа дизайна сайтов',
-    link: 'https://ra-design.jedenkoor.ru',
-    image: '/images/projects/ra-studio.svg',
-    tags: ['Gulp', 'jQuery', 'Лендинг'],
-    date: '2020.03.03'
-  },
-  {
-    title: 'RA-Studio',
-    description: 'Управление репутацией бренда',
-    link: 'https://ra-reputation.jedenkoor.ru',
-    image: '/images/projects/ra-studio.svg',
-    tags: ['Gulp', 'jQuery', 'Лендинг'],
-    date: '2020.06.25'
-  },
-  {
-    title: 'Bo-Box',
-    description: 'Мягкая мебель',
-    link: 'https://nordic.jedenkoor.ru/',
-    image: '/images/projects/bo-box.svg',
-    tags: ['Gulp', 'jQuery', 'Лендинг'],
-    date: '2020.04.24'
-  },
-  {
-    title: 'RA-Studio',
-    description: 'Цифровизация бизнеса',
-    link: 'https://ra-digital.jedenkoor.ru',
-    image: '/images/projects/ra-studio.svg',
-    tags: ['Gulp', 'jQuery', 'Лендинг'],
-    date: '2020.05.05'
-  },
-  {
-    title: 'Innes',
-    description: 'Косметика',
-    link: 'https://innes.jedenkoor.ru/',
-    image: '/images/projects/innes.svg',
-    tags: ['Gulp', 'jQuery', 'Интернет-магазин'],
-    date: '2020.06.08'
-  },
-  {
-    title: 'ITProgress',
-    description: 'Программные внедрения для бизнеса',
-    link: 'https://itprogress.jedenkoor.ru/',
-    image: '/images/projects/itprogress.png',
-    tags: ['Gulp', 'jQuery', 'Корпоративный сайт'],
-    date: '2020.01.21'
-  },
-  {
-    title: 'Райффайзен Банк',
-    description: 'Регистрация на конференцию Fintech Design Conf',
-    link: 'https://fintech.jedenkoor.ru/',
-    image: '/images/projects/fintech.svg',
-    tags: ['Gulp', 'ES6', 'Лендинг', 'Анимации'],
-    date: '2020.08.20'
-  },
-  {
-    title: 'Красные медведи',
-    description: 'Хоккейная экипировка',
-    link: 'https://red-bears.jedenkoor.ru/',
-    image: '/images/projects/red-bears.png',
-    tags: ['Gulp', 'ES6', 'Интернет-магазин'],
-    date: '2020.09.14'
-  },
-  {
-    title: 'My Tobacco',
-    description: 'Кальяны',
-    link: 'https://hookahs.jedenkoor.ru/',
-    image: '/images/projects/hookahs.jpg',
-    tags: ['Gulp', 'ES6', 'Интернет-магазин'],
-    date: '2020.09.28'
-  },
-  {
-    title: 'Райффайзен Банк',
-    description: 'Конференция Fintech Design Conf',
-    link: 'https://raiffeisen.jedenkoor.ru/',
-    image: '/images/projects/fintech.svg',
-    tags: ['Gulp', 'ES6', 'Лендинг', 'Анимации', 'Canvas'],
-    date: '2020.10.23'
-  },
+  // {
+  //   title: 'Internation',
+  //   description: 'Спортивный бренд одежды',
+  //   link: 'https://internation.jedenkoor.ru/',
+  //   image: '/images/projects/internation.svg',
+  //   tags: ['Gulp', 'jQuery', 'Интернет-магазин'],
+  //   date: '2019.03.25'
+  // },
+  // {
+  //   title: 'Малина',
+  //   description: 'Художественная мастерская по созданию и росписи матрешёк',
+  //   link: 'https://matreshki.jedenkoor.ru/',
+  //   image: '/images/projects/matreshki.png',
+  //   tags: ['Gulp', 'jQuery', 'Сайт-визитка'],
+  //   date: '2019.02.18'
+  // },
+  // {
+  //   title: 'Clevergrad Premium',
+  //   description: 'Элитная загородная недвижимость',
+  //   link: 'https://clever-elite.jedenkoor.ru/',
+  //   image: '/images/projects/clever-elite.svg',
+  //   tags: ['Gulp', 'jQuery', 'Интернет-витрина', 'Анимации'],
+  //   date: '2019.05.30'
+  // },
+  // {
+  //   title: 'Instafilter',
+  //   description: 'Студия разработки AR игр и масок для Instagram, TikTok, Snapchat и VK',
+  //   link: 'https://instafilter.jedenkoor.ru/',
+  //   image: '/images/projects/instafilter.svg',
+  //   tags: ['Gulp', 'jQuery', 'Сайт-визитка', 'Анимации'],
+  //   date: '2019.08.13'
+  // },
+  // {
+  //   title: 'Levitacia',
+  //   description: 'Премиальная одежда',
+  //   link: 'https://levitation.jedenkoor.ru/',
+  //   image: '/images/projects/levitacia.png',
+  //   tags: ['Gulp', 'jQuery', 'Интернет-магазин'],
+  //   date: '2019.10.18'
+  // },
+  // {
+  //   title: 'RA-Studio',
+  //   description: 'Продажа дизайна сайтов',
+  //   link: 'https://ra-design.jedenkoor.ru',
+  //   image: '/images/projects/ra-studio.svg',
+  //   tags: ['Gulp', 'jQuery', 'Лендинг'],
+  //   date: '2020.03.03'
+  // },
+  // {
+  //   title: 'RA-Studio',
+  //   description: 'Управление репутацией бренда',
+  //   link: 'https://ra-reputation.jedenkoor.ru',
+  //   image: '/images/projects/ra-studio.svg',
+  //   tags: ['Gulp', 'jQuery', 'Лендинг'],
+  //   date: '2020.06.25'
+  // },
+  // {
+  //   title: 'Bo-Box',
+  //   description: 'Мягкая мебель',
+  //   link: 'https://nordic.jedenkoor.ru/',
+  //   image: '/images/projects/bo-box.svg',
+  //   tags: ['Gulp', 'jQuery', 'Лендинг'],
+  //   date: '2020.04.24'
+  // },
+  // {
+  //   title: 'RA-Studio',
+  //   description: 'Цифровизация бизнеса',
+  //   link: 'https://ra-digital.jedenkoor.ru',
+  //   image: '/images/projects/ra-studio.svg',
+  //   tags: ['Gulp', 'jQuery', 'Лендинг'],
+  //   date: '2020.05.05'
+  // },
+  // {
+  //   title: 'Innes',
+  //   description: 'Косметика',
+  //   link: 'https://innes.jedenkoor.ru/',
+  //   image: '/images/projects/innes.svg',
+  //   tags: ['Gulp', 'jQuery', 'Интернет-магазин'],
+  //   date: '2020.06.08'
+  // },
+  // {
+  //   title: 'ITProgress',
+  //   description: 'Программные внедрения для бизнеса',
+  //   link: 'https://itprogress.jedenkoor.ru/',
+  //   image: '/images/projects/itprogress.png',
+  //   tags: ['Gulp', 'jQuery', 'Корпоративный сайт'],
+  //   date: '2020.01.21'
+  // },
+  // {
+  //   title: 'Райффайзен Банк',
+  //   description: 'Регистрация на конференцию Fintech Design Conf',
+  //   link: 'https://fintech.jedenkoor.ru/',
+  //   image: '/images/projects/fintech.svg',
+  //   tags: ['Gulp', 'ES6', 'Лендинг', 'Анимации'],
+  //   date: '2020.08.20'
+  // },
+  // {
+  //   title: 'Красные медведи',
+  //   description: 'Хоккейная экипировка',
+  //   link: 'https://red-bears.jedenkoor.ru/',
+  //   image: '/images/projects/red-bears.png',
+  //   tags: ['Gulp', 'ES6', 'Интернет-магазин'],
+  //   date: '2020.09.14'
+  // },
+  // {
+  //   title: 'My Tobacco',
+  //   description: 'Кальяны',
+  //   link: 'https://hookahs.jedenkoor.ru/',
+  //   image: '/images/projects/hookahs.jpg',
+  //   tags: ['Gulp', 'ES6', 'Интернет-магазин'],
+  //   date: '2020.09.28'
+  // },
+  // {
+  //   title: 'Райффайзен Банк',
+  //   description: 'Конференция Fintech Design Conf',
+  //   link: 'https://raiffeisen.jedenkoor.ru/',
+  //   image: '/images/projects/fintech.svg',
+  //   tags: ['Gulp', 'ES6', 'Лендинг', 'Анимации', 'Canvas'],
+  //   date: '2020.10.23'
+  // },
   {
     title: 'Moire',
     description: 'Дипломный проект SkillBox на Vue.js',
